b1f343e67843349c1b859a0bc9706ea47774753d
Lexy Kramer <lexy.kramer@gmail.com>
2021-07-07 13:14:27 -0400

    Initialized client and server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.listen(PORT, () => {
+  console.log(`server now running on http://localhost:${PORT}`);
+});
