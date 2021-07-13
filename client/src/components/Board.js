2c3e6f4c176c63c9a7eaa90f408be236c78ee88d
Lexy Kramer <lexy.kramer@gmail.com>
2021-07-13 13:01:36 -0400

    All helper tests passing, now correctly placing moved tasks

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -2,80 +2,60 @@ import { DragDropContext } from "react-beautiful-dnd";
 import List from "./List";
 import axios from "axios";
 import getOrder from "../helpers/getOrder";
+import moveTask from "../helpers/moveTask";
 
 function Board({ listData, setListData, getUserInfo }) {
   console.log(listData);
   const onDragEnd = async ({ destination, source }) => {
     if (!destination) return;
-    const sourceList = listData.find((list) => list.id === source.droppableId);
-    const destinationList = listData.find(
+
+    let listCopy = JSON.parse(JSON.stringify(listData));
+
+    const sourceList = listCopy.find((list) => list.id === source.droppableId);
+    const destinationList = listCopy.find(
       (list) => list.id === destination.droppableId
     );
 
-    const sourceTaskId = sourceList.Tasks.find(
-      (task) => task.order === source.index
-    ).id;
+    // const sourceTaskId = sourceList.Tasks.find(
+    //   (task) => task.order === source.index
+    // ).id;
 
-    const dTaskIndex = destinationList.Tasks.findIndex(
+    const dIndex = destinationList.Tasks.findIndex(
       (task) => task.order === destination.index
     );
+
     const sTaskIndex = sourceList.Tasks.findIndex(
       (task) => task.order === source.index
     );
-    const dListIndex = listData.findIndex(
+    const dListIndex = listCopy.findIndex(
       (list) => list.id === destination.droppableId
     );
-    const sListIndex = listData.findIndex(
+    const sListIndex = listCopy.findIndex(
       (list) => list.id === source.droppableId
     );
-    const order = getOrder;
 
-    console.log(dTaskIndex);
+    const dTasks = listCopy[dListIndex].Tasks;
 
-    // let order;
-    // if (dTaskIndex === 0) {
-    //   order = destinationList.Tasks[dTaskIndex].order / 2;
-    // } else if (dTaskIndex === -1) {
-    //   if (destinationList.Tasks.length > 0) {
-    //     let lastTaskOrderNum =
-    //       destinationList.Tasks[destinationList.Tasks.length - 1].order;
-    //     order = (Math.floor(lastTaskOrderNum / 100) + 1) * 100;
-    //   } else {
-    //     order = 100;
-    //   }
-    // } else {
-    //   order =
-    //     (destinationList.Tasks[dTaskIndex].order +
-    //       destinationList.Tasks[dTaskIndex - 1].order) /
-    //     2;
-    // }
+    const order = getOrder(dTasks, dIndex);
 
-    // //change order locally
-    // let editListData = [...listData];
-    // //add task to destination list
-    // console.log("=======dListIndex==========");
-    // console.log(dListIndex);
-    // const task = listData[sListIndex].Tasks[sTaskIndex];
-    // task.order = order;
-    // editListData[sListIndex].Tasks.splice(sTaskIndex, 1);
-    // if (dListIndex === -1) {
-    //   editListData[dListIndex].Tasks.splice(
-    //     editListData[dListIndex].Tasks.length + 1,
-    //     0,
-    //     task
-    //   );
-    // } else if (dListIndex === 0) {
-    //   editListData[dListIndex].Tasks.push(task);
-    // } else {
-    //   console.log("HEYYYY");
-    //   editListData[dListIndex].Tasks.splice(dTaskIndex, 0, task);
-    // }
-    // //remove from source list
-    // await setListData(editListData);
+    let task = sourceList.Tasks[sTaskIndex];
+    task.order = order;
 
-    // //change order in database
-    // const URL = `/tasks/${sourceTaskId}/${destination.droppableId}/${order}`;
-    // await axios.patch(URL);
+    //moveTask parameters:
+    //list, dListIndex, sListIndex, dTaskIndex, sTaskIndex, task
+    const editedList = moveTask(
+      listCopy,
+      dListIndex,
+      sListIndex,
+      dIndex,
+      sTaskIndex,
+      task
+    );
+    setListData(editedList);
+
+    //change order in database
+    const URL = `/tasks/${task.id}/${destination.droppableId}/${order}`;
+    await axios.patch(URL);
     // getUserInfo();
   };
 
